a6738d785d73e50486b3c01849bd4ea0a2fefac6
Grimrukh <grimrukh@gmail.com>
2025-01-24 09:49:44 +1000

    Added basic Extensions

---
+++ RoundtableBase/Extensions/Collections.cs
@@ -0,0 +1,22 @@
+﻿namespace RoundtableBase.Extensions;
+
+public static class Collections
+{
+    /// <summary>
+    /// Forgiving version of `List` slicer that automatically clamps the indices to the list bounds.
+    ///
+    /// If `startIndex` is greater than `endIndex`, an empty list is returned.
+    /// </summary>
+    /// <param name="list"></param>
+    /// <param name="startIndex"></param>
+    /// <param name="endIndex"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static List<T> GetRangeSafe<T>(this List<T> list, int startIndex, int endIndex)
+    {
+        endIndex = Math.Min(endIndex, list.Count);
+        return startIndex > endIndex 
+            ? [] 
+            : list.GetRange(startIndex, endIndex - startIndex);
+    }
+}
---
+++ RoundtableBase/Extensions/Memory.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+namespace RoundtableBase.Extensions;
+
+public static class Memory
+{
+    /// <summary>
+    /// Convert a C# struct to its underlying byte array.
+    /// </summary>
+    /// <param name="myStruct"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static byte[] GetBytes<T>(this T myStruct) where T : struct 
+    {
+        int size = Marshal.SizeOf(myStruct);
+        byte[] bytes = new byte[size];
+
+        IntPtr ptr = IntPtr.Zero;
+        try
+        {
+            ptr = Marshal.AllocHGlobal(size);
+            Marshal.StructureToPtr(myStruct, ptr, true);
+            Marshal.Copy(ptr, bytes, 0, size);
+        }
+        finally
+        {
+            Marshal.FreeHGlobal(ptr);
+        }
+        return bytes;
+    }
+}
