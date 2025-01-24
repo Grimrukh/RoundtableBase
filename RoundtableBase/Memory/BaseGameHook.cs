81901762edc90ba66b505337950f520cf003dcc4
Grimrukh <grimrukh@gmail.com>
2025-01-24 11:50:01 +1000

    Added BaseGameHook, RTPointer, Keystone

---
+++ RoundtableBase/Memory/BaseGameHook.cs
@@ -0,0 +1,189 @@
+﻿using Keystone;
+using PropertyHook;
+
+namespace RoundtableBase.Memory;
+
+public abstract class BaseGameHook : PHook
+{
+    public IntPtr MainModuleBaseAddress { get; private set; }
+    protected static readonly Engine Engine = new(Architecture.X86, Mode.X64);
+
+    public bool Focused => Hooked && User32.GetForegroundProcessID() == Process.Id;
+
+    public long GameLoadedTimeMs { get; protected set; } = -1;  // default is unloaded
+
+    public bool IsGameLoaded => GameLoadedTimeMs >= 0;  // i.e. not -1
+    
+    /// <summary>
+    /// Event that fires when the game is loaded. Firing trigger is game-specific (e.g. `PlayerIns` is valid).
+    /// </summary>
+    public event EventHandler<PHEventArgs>? OnGameLoaded;
+    
+    /// <summary>
+    /// Event that fires when the game is loaded. Firing trigger is game-specific (e.g. `PlayerIns` is invalid).
+    /// </summary>
+    public event EventHandler<PHEventArgs>? OnGameUnloaded;
+
+    public BaseGameHook(int refreshInterval, int minLifetime, string windowTitle) :
+        base(refreshInterval, minLifetime, p => p.MainWindowTitle == windowTitle)
+    {
+        OnHooked += OnHookedHandler;
+        OnUnhooked += OnUnhookedHandler;
+    }
+
+    protected virtual void OnHookedHandler(object? sender, PHEventArgs e)
+    {
+        // Record main module base address.
+        if (Process.MainModule != null) 
+            MainModuleBaseAddress = Process.MainModule.BaseAddress;
+    }
+
+    protected virtual void OnUnhookedHandler(object? sender, PHEventArgs e)
+    {
+        // No base logic.
+    }
+    
+    public override void Refresh()
+    {
+        base.Refresh();
+        if (!Hooked)
+            return;
+        bool isGameLoadedNow = CheckIsGameLoaded();
+        switch (IsGameLoaded)
+        {
+            case false when isGameLoadedNow:
+                // Game has just loaded.
+                OnGameLoaded?.Invoke(this, new PHEventArgs(this));
+                GameLoadedTimeMs = 0;
+                break;
+            case true when !isGameLoadedNow:
+                // Game has just unloaded. Note that this CANNOT trigger before `OnGameLoaded` triggers, as IsGameLoaded
+                // will only be set to true when the hook first sees that the game is loaded.
+                OnGameUnloaded?.Invoke(this, new PHEventArgs(this));
+                GameLoadedTimeMs = -1;
+                break;
+            case true:
+                // Still loaded. Increment loaded time.
+                GameLoadedTimeMs += RefreshInterval;
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Must be implemented by derived classes to check if the game is loaded.
+    /// </summary>
+    /// <returns></returns>
+    protected abstract bool CheckIsGameLoaded();
+
+    /// <summary>
+    /// Convert to array of ints for AOBScanner.
+    /// </summary>
+    /// <param name="bytes"></param>
+    /// <returns></returns>
+    public static int[] BytesToPattern(byte[] bytes)
+    {
+        int[] pattern = new int[bytes.Length];
+        for (int i = 0; i < bytes.Length; i++)
+            pattern[i] = bytes[i];
+        return pattern;
+    }
+    
+    public bool AssembleAndExecute(string asm)
+    {
+        //Console.WriteLine(asm);
+        
+        if (Process.MainModule == null)
+            return false;
+        
+        // Assemble from base address to get array size.
+        EncodedData? bytes = Engine.Assemble(asm, (ulong)Process.MainModule.BaseAddress);
+        KeystoneError error = Engine.GetLastKeystoneError();
+        if (error != KeystoneError.KS_ERR_OK)
+            throw new Exception($"Something went wrong during assembly. Code could not be assembled. Error: {error}");
+
+        IntPtr? startPtr = null;
+        try
+        {
+            startPtr = AllocateClose(bytes.Buffer.Length, flProtect: Kernel32.PAGE_EXECUTE_READWRITE);
+            // Reassemble with the location of `startPtr` to support relative instructions.
+            bytes = Engine.Assemble(asm, (ulong)startPtr);
+            error = Engine.GetLastKeystoneError();
+            if (error != KeystoneError.KS_ERR_OK) // would be very unusual
+            {
+                Logging.Debug($"Something went wrong during assembly. Code could not be assembled. Error: {error}");
+                return false;
+            }
+            Kernel32.WriteBytes(Handle, startPtr.Value, bytes.Buffer);
+            //DebugPrintArray(bytes.Buffer);
+            Execute(startPtr.Value);
+        }
+        catch (Exception e)
+        {
+            Logging.Debug($"Failed to assemble and execute code: {e.Message}");
+            return false;
+        }
+        finally
+        {
+            if (startPtr != null)
+                Free(startPtr.Value);
+        }
+        return true;
+    }
+
+    public bool AssembleAndInject(string asm, IntPtr address, int injectionSiteLength, IntPtr? basePtr = null)
+    {
+        if (Process.MainModule == null)
+            return false;
+        if (basePtr == null)
+            basePtr = Process.MainModule.BaseAddress;
+        
+        EncodedData? bytes = Engine.Assemble(asm, (ulong)basePtr);
+        KeystoneError error = Engine.GetLastKeystoneError();
+        if (error != KeystoneError.KS_ERR_OK)
+            throw new Exception($"Something went wrong during assembly. Code could not be assembled. Error: {error}");
+
+        if (bytes.Buffer.Length < injectionSiteLength)
+        {
+            // Add padding "nop" instructions to `asm` and reassemble.
+            int paddingLength = injectionSiteLength - bytes.Buffer.Length;
+            for (int i = 0; i < paddingLength; i++)
+                asm += "\nnop";
+            bytes = Engine.Assemble(asm, (ulong)basePtr);
+            error = Engine.GetLastKeystoneError();
+            if (error != KeystoneError.KS_ERR_OK)
+                throw new Exception($"Something went wrong during assembly. Code could not be assembled. Error: {error}");
+        }
+        
+        Kernel32.WriteBytes(Handle, address, bytes.Buffer);
+        //DebugPrintArray(bytes.Buffer);
+        
+        return true;
+    }
+    
+    public IntPtr AssembleAllocateWrite(string asm, IntPtr? basePtr = null, bool executable = true)
+    {
+        //Console.WriteLine(asm);
+        
+        if (Process.MainModule == null)
+            return IntPtr.Zero;
+        
+        // Assemble from base address to get array size.
+        EncodedData? bytes = Engine.Assemble(asm, (ulong)Process.MainModule.BaseAddress);
+        KeystoneError error = Engine.GetLastKeystoneError();
+        if (error != KeystoneError.KS_ERR_OK)
+            throw new Exception($"Something went wrong during assembly. Code could not be assembled. Error: {error}");
+
+        IntPtr startPtr = AllocateClose(bytes.Buffer.Length, basePtr, flProtect: executable ? Kernel32.PAGE_EXECUTE_READWRITE : Kernel32.PAGE_READWRITE);
+
+        // Reassemble with the location of `startPtr` to support relative instructions.
+        bytes = Engine.Assemble(asm, (ulong)startPtr);
+        error = Engine.GetLastKeystoneError();
+        if (error != KeystoneError.KS_ERR_OK)  // would be very unusual
+            throw new Exception($"Something went wrong during assembly. Code could not be assembled. Error: {error}");
+
+        Kernel32.WriteBytes(Handle, startPtr, bytes.Buffer);
+        //DebugPrintArray(bytes.Buffer);
+        
+        return startPtr;
+    }
+}
---
+++ RoundtableBase/Memory/RTPointer.cs
@@ -0,0 +1,234 @@
+﻿using System.Text;
+
+using PropertyHook;
+
+namespace RoundtableBase.Memory;
+
+/// <summary>
+/// Wraps a `PHPointer` (or child thereof) from `PropertyHook` for use as a base class for Roundtable in-memory classes.
+/// </summary>
+/// <param name="pointer"></param>
+public abstract class RTPointer(PHPointer pointer)
+{
+    public PHPointer Pointer { get; } = pointer;
+
+    public bool IsValid => Pointer.IsNonZero;
+    
+    /// <summary>
+    /// Create a child pointer with the given nested sequence of offsets.
+    /// </summary>
+    /// <param name="offsets"></param>
+    /// <returns></returns>
+    public PHPointer CreateChildPointer(params int[] offsets)
+    {
+        return Pointer.CreateChildPointer(offsets);
+    }
+    
+    /// <summary>
+    /// Read `length` number of bytes from the given offset.
+    /// </summary>
+    public byte[] ReadBytes(int offset, uint length)
+    {
+        return Pointer.ReadBytes(offset, length);
+    }
+    /// <summary>
+    /// Write `length` number of bytes at the given offset.
+    /// </summary>
+    public bool WriteBytes(int offset, byte[] bytes)
+    {
+        return Pointer.WriteBytes(offset, bytes);
+    }
+    /// <summary>
+    /// Read an address from the given offset. (There is no Write version of this method.)
+    /// </summary>
+    public IntPtr ReadIntPtr(int offset)
+    {
+        return Pointer.ReadIntPtr(offset);
+    }
+    /// <summary>
+    /// Read a 4-byte bitfield and return whether the bit specified by the given mask is set.
+    /// </summary>
+    public bool ReadFlag32(int offset, uint mask)
+    {
+        return Pointer.ReadFlag32(offset, mask);
+    }
+    /// <summary>
+    /// Set the state of a bit specified by the given mask in a 4-byte bitfield.
+    /// </summary>
+    public bool WriteFlag32(int offset, uint mask, bool state)
+    {
+        return Pointer.WriteFlag32(offset, mask, state);
+    }
+    /// <summary>
+    /// Read a 1-byte signed integer.
+    /// </summary>
+    public sbyte ReadSByte(int offset)
+    {
+        return Pointer.ReadSByte(offset);
+    }
+    /// <summary>
+    /// Write a 1-byte signed integer.
+    /// </summary>
+    public bool WriteSByte(int offset, sbyte value)
+    {
+        return Pointer.WriteSByte(offset, value);
+    }
+    /// <summary>
+    /// Read a 1-byte unsigned integer.
+    /// </summary>
+    public byte ReadByte(int offset)
+    {
+        return Pointer.ReadByte(offset);
+    }
+    /// <summary>
+    /// Write a 1-byte unsigned integer.
+    /// </summary>
+    public bool WriteByte(int offset, byte value)
+    {
+        return Pointer.WriteByte(offset, value);
+    }
+    /// <summary>
+    /// Read a 1-byte boolean value.
+    /// </summary>
+    public bool ReadBoolean(int offset)
+    {
+        return Pointer.ReadBoolean(offset);
+    }
+    /// <summary>
+    /// Write a 1-byte boolean value.
+    /// </summary>
+    public bool WriteBoolean(int offset, bool value)
+    {
+        return Pointer.WriteBoolean(offset, value);
+    }
+    /// <summary>
+    /// Read a 2-byte signed integer.
+    /// </summary>
+    public short ReadInt16(int offset)
+    {
+        return Pointer.ReadInt16(offset);
+    }
+    /// <summary>
+    /// Write a 2-byte signed integer.
+    /// </summary>
+    public bool WriteInt16(int offset, short value)
+    {
+        return Pointer.WriteInt16(offset, value);
+    }
+    /// <summary>
+    /// Read a 2-byte unsigned integer.
+    /// </summary>
+    public ushort ReadUInt16(int offset)
+    {
+        return Pointer.ReadUInt16(offset);
+    }
+    /// <summary>
+    /// Write a 2-byte unsigned integer.
+    /// </summary>
+    public bool WriteUInt16(int offset, ushort value)
+    {
+        return Pointer.WriteUInt16(offset, value);
+    }
+    /// <summary>
+    /// Read a 4-byte signed integer.
+    /// </summary>
+    public int ReadInt32(int offset)
+    {
+        return Pointer.ReadInt32(offset);
+    }
+    /// <summary>
+    /// Write a 4-byte signed integer.
+    /// </summary>
+    public bool WriteInt32(int offset, int value)
+    {
+        return Pointer.WriteInt32(offset, value);
+    }
+    /// <summary>
+    /// Read a 4-byte unsigned integer.
+    /// </summary>
+    public uint ReadUInt32(int offset)
+    {
+        return Pointer.ReadUInt32(offset);
+    }
+    /// <summary>
+    /// Write a 4-byte unsigned integer.
+    /// </summary>
+    public bool WriteUInt32(int offset, uint value)
+    {
+        return Pointer.WriteUInt32(offset, value);
+    }
+    /// <summary>
+    /// Read an 8-byte signed integer.
+    /// </summary>
+    public long ReadInt64(int offset)
+    {
+        return Pointer.ReadInt64(offset);
+    }
+    /// <summary>
+    /// Write an 8-byte signed integer.
+    /// </summary>
+    public bool WriteInt64(int offset, long value)
+    {
+        return Pointer.WriteInt64(offset, value);
+    }
+    /// <summary>
+    /// Read an 8-byte unsigned integer.
+    /// </summary>
+    public ulong ReadUInt64(int offset)
+    {
+        return Pointer.ReadUInt64(offset);
+    }
+    /// <summary>
+    /// Write an 8-byte unsigned integer.
+    /// </summary>
+    public bool WriteUInt64(int offset, ulong value)
+    {
+        return Pointer.WriteUInt64(offset, value);
+    }
+    /// <summary>
+    /// Read a 4-byte floating point number.
+    /// </summary>
+    public float ReadSingle(int offset)
+    {
+        return Pointer.ReadSingle(offset);
+    }
+    /// <summary>
+    /// Write a 4-byte floating point number.
+    /// </summary>
+    public bool WriteSingle(int offset, float value)
+    {
+        return Pointer.WriteSingle(offset, value);
+    }
+    /// <summary>
+    /// Read an 8-byte floating point number.
+    /// </summary>
+    public double ReadDouble(int offset)
+    {
+        return Pointer.ReadDouble(offset);
+    }
+    /// <summary>
+    /// Write an 8-byte floating point number.
+    /// </summary>
+    public bool WriteDouble(int offset, double value)
+    {
+        return Pointer.WriteDouble(offset, value);
+    }
+    /// <summary>
+    /// Reads and decodes a fixed amount of bytes as a string.
+    ///
+    /// If `trim` is set, terminates after the first null character.
+    /// </summary>
+    public string ReadString(int offset, Encoding encoding, uint byteCount, bool trim = true)
+    {
+        return Pointer.ReadString(offset, encoding, byteCount, trim);
+    }
+    /// <summary>
+    /// Encodes and writes a string as a fixed amount of bytes.
+    ///
+    /// If the string is too long to fit, it is truncated before encoding.
+    /// </summary>
+    public bool WriteString(int offset, Encoding encoding, uint byteCount, string value)
+    {
+        return Pointer.WriteString(offset, encoding, byteCount, value);
+    }
+}
