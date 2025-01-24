093a61f983c9ab90d33f519cc49f5999ebe3957b
Grimrukh <grimrukh@gmail.com>
2025-01-24 10:03:22 +1000

    Added SF subrepo and conditional flat/SA include

---
+++ RoundtableBase/Extensions/Events.cs
@@ -0,0 +1,23 @@
+﻿using SoulsFormats;
+
+namespace RoundtableBase.Extensions;
+
+public static class Events
+{
+    /// <summary>
+    /// Retrieve an Event in `EMEVD` by its event ID, or create and return a new empty Event if it doesn't exist.
+    /// </summary>
+    /// <param name="emevd"></param>
+    /// <param name="eventID"></param>
+    /// <returns></returns>
+    public static EMEVD.Event FindOrCreateEventID(this EMEVD emevd, long eventID)
+    {
+        EMEVD.Event? ev = emevd.Events.Find(e => e.ID == eventID);
+        if (ev != null)
+            return ev;
+        
+        ev = new EMEVD.Event(eventID);
+        emevd.Events.Add(ev);
+        return ev;
+    }    
+}
